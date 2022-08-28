1c583bf03adf84ec6475b352f8af7ea695d26a6c
karwa <karwatmateusz07@gmail.com>
2022-08-28 10:54:20 +0200

    update file structure

--- src/ex2.py
+++
@@ -1,8 +0,0 @@
-file = open("ex2.txt", "w")
-for i in range(100):
-    file.write(f'{i};\n')
-file.close()
-
-file = open("ex2.txt", "r")
-print(file.read())
-file.close()
--- src/ex3.py
+++
@@ -1,9 +0,0 @@
-file = open("ex3.txt", "w")
-for i in range(100):
-    if (i%3==0 ) and (i%6==0):
-        file.write(f'{i};\n')
-file.close()
-
-file = open("ex3.txt", "r")
-print(file.read())
-file.close()
