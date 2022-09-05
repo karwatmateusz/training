611e334561d103a46d4891f3a36c7270820235bd
karwa <karwatmateusz07@gmail.com>
2022-09-05 08:49:41 +0200

    final solution + things to discuss

--- src/L4/ex1.py
+++ src/L4/ex1.py
@@ -1,4 +1,3 @@
-
 def not_divide_by_zero(func):
     def wrapper(a, b):
         if b != 0:
@@ -6,17 +5,32 @@ def not_divide_by_zero(func):
             return result
         else:
             raise ZeroDivisionError("Dzielenie przez zero zabronione")
+
     return wrapper
 
-# def not_divide_by_zero_catch(func):
-#     def wrapper():
-#         try:
-            
 
-@not_divide_by_zero
+def not_divide_by_zero_catch(func):
+    def wrapper(a, b):
+        try:
+            result = func(a, b)
+            return result
+        except ZeroDivisionError as e:
+            print(f"{e}, nie mozesz dzielic przez zero")
+
+    return wrapper
+
+
+# @not_divide_by_zero
+@not_divide_by_zero_catch
 def divide(a, b):
     result = a / b
     return result
 
-x = divide(4, 2)
+
+x = divide(4, 0) if divide(4, 0) is not None else "Brak wyniku"
+# to discuss
+# x = divide(4,0)
+# if x is None:
+#     x = 'Brak wyniku'
+
 print(x)
