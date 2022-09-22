6e43108925ee8513483b3e08e46804d8964ada56
Mateusz Karwat <mkarw@softserveinc.com>
2022-09-22 09:37:26 +0200

    wait improvement

--- src/ATframework/pages/LoginPage.py
+++ src/ATframework/pages/LoginPage.py
@@ -7,8 +7,8 @@ class LoginPage(BasePageClass):
 
     _URL = "https://demo.opencart.com/index.php?route=account/login"
     email_locator = (By.CSS_SELECTOR, '#input-email')
-    password_locator = ''
-    login_locator = ''
+    password_locator = (By.CSS_SELECTOR, '#input-password')
+    login_locator = (By.CSS_SELECTOR, '#form-login > button')
 
     def __init__(self, driver):
         self.driver = driver
@@ -17,13 +17,17 @@ class LoginPage(BasePageClass):
     #     self.driver.get(self._URL)
     
     def email_input(self, email):
-        email_field = self.driver.find_element(self.email_locator[0], self.email_locator[1])
+        # self.verify_presence(self.email_locator[0], self.email_locator[1])
+        # email_field = self.driver.find_element(self.email_locator[0], self.email_locator[1])
+        email_field = self.verify_element(self.email_locator)
         email_field.send_keys(email)
 
     def password_input(self, password):
-        password_field = self.driver.find_element(By.CSS_SELECTOR, self.password_locator)
+        # password_field = self.driver.find_element(By.CSS_SELECTOR, self.password_locator)
+        password_field = self.verify_element(self.password_locator)
         password_field.send_keys(password)
     
     def login_button_click(self):
-        login_button = self.driver.find_element(By.CSS_SELECTOR, self.login_locator)
+        # login_button = self.driver.find_element(By.CSS_SELECTOR, self.login_locator)
+        login_button = self.verify_element(self.login_locator)
         login_button.click()
--- src/ATframework/utilities/BasePageClass.py
+++ src/ATframework/utilities/BasePageClass.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class BasePageClass:
     
     _URL = None
@@ -5,3 +8,8 @@ class BasePageClass:
     def go(self):
         #add wait for page loading
         self.driver.get(self._URL)
+
+    def verify_element(self, locator):
+        return WebDriverWait(self.driver, timeout=5).until(EC.visibility_of_element_located((locator[0], locator[1])))
+        # element = self.driver.find_element(locator[0], locator[1])
+        # return element
